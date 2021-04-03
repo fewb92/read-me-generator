fc80744c23eac970c8403cb1ac7f889d7f8a5e39
JSoto <sotojosh96@gmail.com>
2021-04-03 19:54:32 -0400

    added git ignor and did more editing of the layout and formatting

--- index.js
+++ index.js
@@ -67,51 +67,49 @@ inquirer
 * [Email](#Email)
 
 
-
-
 <a name="#Description"></a>
 ## Description:
-${answers.description}
+    ${answers.description}
 
 <a name="#Usage"></a>
 ## Usage
-${answers.usage}
+    ${answers.usage}
 
 <a name="#Contribution"></a>
 ## Contribution Guidelines
-${answers.contributing}
+    ${answers.contributing}
 
 <a name="#Tests"></a>
 ## Tests
-Some of the tests that I ran for this project are: ${answers.tests}.
+    Some of the tests that I ran for this project are: ${answers.tests}.
 
 <a name="#License"></a>
 ## License
-MIT License
-Copyright (c) 2021 ${answers.name}
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    MIT License
+    Copyright (c) 2021 ${answers.name}
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
 <a name="#GitHub"></a>
 ## Github
-Please visit my ${answers.github} to see my work.
+Please visit my [github](${answers.github}) to see my work.
 
 <a name="#Email"></a>
 ## Email
-My Email is ${answers.email}. Feel free to reach me here.`);
+    My Email is ${answers.email}. Feel free to reach me here.`);
 
 
         // console.log("this is the name", name)
